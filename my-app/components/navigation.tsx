9961c25f0abd0772d1105833bb54ce9fa4ad051c
MHUZAIFA <mhuzaifa.career@outlook.com>
2025-12-28 13:52:32 -0500

    removed redundant tech section

--- my-app/components/navigation.tsx
+++ my-app/components/navigation.tsx
@@ -9,7 +9,6 @@ import { cn } from "@/lib/utils";
 const navItems = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
-  { href: "/technologies", label: "Technologies" },
   { href: "/experience", label: "Experience" },
   { href: "/projects", label: "Projects" },
   { href: "/certificates", label: "Certificates" },
